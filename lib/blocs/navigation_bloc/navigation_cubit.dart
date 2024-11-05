b65805d415e8be14766a0672e6d5d4c1c3f36880
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-05 15:27:29 +0100

    feat: adding new navigation states

--- lib/blocs/navigation_bloc/navigation_cubit.dart
+++ lib/blocs/navigation_bloc/navigation_cubit.dart
@@ -4,7 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class NavigationCubit extends Cubit<NavigationState> {
   NavigationCubit() : super(Home());
 
+  //? Available in bottom navigation bar
   void showHomeView() => emit(Home());
   void showPlanningView() => emit(Planning());
   void showCurrentMissionView() => emit(CurrentMission());
+
+  //? Available in sidebar
+  void showProfileView() => emit(Profile());
+  void showMissionSwap() => emit(MissionSwap());
+  void showSettings() => emit(Settings());
+  void showHelp() => emit(Help());
 }
--- lib/blocs/navigation_bloc/navigation_state.dart
+++ lib/blocs/navigation_bloc/navigation_state.dart
@@ -5,3 +5,11 @@ class Home extends NavigationState {}
 class Planning extends NavigationState {}
 
 class CurrentMission extends NavigationState {}
+
+class Profile extends NavigationState {}
+
+class MissionSwap extends NavigationState {}
+
+class Settings extends NavigationState {}
+
+class Help extends NavigationState {}
