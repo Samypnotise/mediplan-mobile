424b73aadd134efb266daa56d0bf43ae973836b5
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-18 22:01:37 +0100

    chore: loading .env in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mediplan/repositories/auth_repository.dart';
 import 'package:mediplan/views/Navigation/app_navigator.dart';
 import 'package:mediplan/blocs/session_bloc/session_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(const Mediplan());
 }
 
