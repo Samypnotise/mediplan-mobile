a60b1ce3b65c3d07b58cb49fdb0d9b7a7fca1616
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-18 22:04:27 +0100

    style: lint
    Ordering imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:mediplan/repositories/auth_repository.dart';
-import 'package:mediplan/views/Navigation/app_navigator.dart';
-import 'package:mediplan/blocs/session_bloc/session_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mediplan/blocs/session_bloc/session_cubit.dart';
+import 'package:mediplan/repositories/auth_repository.dart';
+import 'package:mediplan/views/Navigation/app_navigator.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
