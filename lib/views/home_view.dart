0871e420ac26fa8cfb4c41f7bed0b58ffe4bde75
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-27 19:12:30 +0100

    feat: adding tiles to home view
    - Number of missions today
    - Next mission in ...

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,6 +10,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double phoneWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -58,12 +60,97 @@ class HomeView extends StatelessWidget {
                 ],
               ),
 
-              //! Nombre de missions de la journée dans une tile
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //! Nombre de missions de la journée dans une tile
+                    Container(
+                      height: 110,
+                      width: phoneWidth * 0.45,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: MediplanColors.quaternary,
+                            spreadRadius: 1,
+                            blurRadius: 4,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
+                        child: InkWell(
+                          splashColor: MediplanColors.quaternary,
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+                            context.read<NavigationCubit>().showPlanningView();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 15,
+                              bottom: 10,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                //! Nombre de missions
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.baseline,
+                                  textBaseline: TextBaseline.alphabetic,
+                                  children: [
+                                    Text(
+                                      "10", // TODO : Inclure le nombre de missions à partir du state Mediplan
+                                      style: GoogleFonts.sourceSansPro(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 40,
+                                        color: MediplanColors.secondary,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 5),
+                                      child: Text(
+                                        "missions", // TODO : Inclure le nombre de missions à partir du state Mediplan
+                                        style: GoogleFonts.sourceSansPro(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 22,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                //! Hint
+                                Text(
+                                  "Cliquez pour y accéder",
+                                  style: GoogleFonts.sourceSansPro(
+                                    fontWeight: FontWeight.bold,
+                                    color: MediplanColors.placeholder,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //! Prochaine missions dans : (nombre d'heures et minutes)
               Padding(
                 padding: const EdgeInsets.only(top: 20),
                 child: Container(
                   height: 150,
-                  width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
@@ -80,31 +167,38 @@ class HomeView extends StatelessWidget {
                     color: Colors.transparent,
                     borderRadius: BorderRadius.circular(20),
                     child: InkWell(
-                      splashColor: MediplanColors.quaternary,
                       borderRadius: BorderRadius.circular(20),
+                      splashColor: MediplanColors.quaternary,
                       onTap: () {
-                        context.read<NavigationCubit>().showPlanningView();
+                        context
+                            .read<NavigationCubit>()
+                            .showCurrentMissionView();
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(20),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              "Nombre de missions aujourd'hui",
-                              style: GoogleFonts.sourceSansPro(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-                            Text(
-                              "5", // TODO : Inclure le nombre de missions à partir du state Mediplan
-                              style: GoogleFonts.sourceSansPro(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                color: MediplanColors.secondary,
-                              ),
+                            //! Titre de la tile
+                            Row(
+                              children: [
+                                const FaIcon(
+                                  FontAwesomeIcons.truckMedical,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10),
+                                  child: Text(
+                                    "Prochaine mission",
+                                    style: GoogleFonts.sourceSansPro(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                             Text(
                               "Cliquez pour y accéder",
@@ -122,8 +216,6 @@ class HomeView extends StatelessWidget {
                   ),
                 ),
               ),
-
-              //! Prochaine missions dans : (nombre d'heures et minutes)
 
               //! Demandes de changements de missions reçues
             ],
