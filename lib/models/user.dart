1eea735a552c323acf6044c4a2ec3ab94f8c52b5
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-27 19:10:50 +0100

    feat: making the users searchable in a list

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,22 +1,30 @@
-class User {
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
+
+class User with CustomDropdownListFilter {
   final String id;
-  // final String firstName;
-  // final String lastName;
+  final String firstName;
+  final String lastName;
   final String email;
 
   User({
     required this.id,
-    // required this.firstName,
-    // required this.lastName,
+    required this.firstName,
+    required this.lastName,
     required this.email,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
-      // firstName: json['firstName'],
-      // lastName: json['lastName'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
       email: json['email'],
     );
+  }
+
+  //? Allows this object to be searchable in a dropdown
+  @override
+  bool filter(String query) {
+    return lastName.toLowerCase().contains(query.toLowerCase());
   }
 }
