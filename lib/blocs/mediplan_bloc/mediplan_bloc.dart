50779950c535ca727d68ac871c119bfb2939356b
Samypnotise <SamyDjemiliSD@gmail.com>
2024-12-08 19:21:24 +0100

    feat: fetching missions

--- lib/blocs/mediplan_bloc/mediplan_bloc.dart
+++ lib/blocs/mediplan_bloc/mediplan_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:mediplan/blocs/mediplan_bloc/mediplan_event.dart';
 import 'package:mediplan/blocs/mediplan_bloc/mediplan_state.dart';
 import 'package:mediplan/blocs/mediplan_bloc/mediplan_status.dart';
+import 'package:mediplan/models/mission.dart';
 import 'package:mediplan/models/user.dart';
 import 'package:mediplan/repositories/mediplan_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,6 +23,66 @@ class MediplanBloc extends Bloc<MediplanEvent, MediplanState> {
     print("fetching user data ... TODO HERE");
 
     await attemptFetchingCaregivers(emit);
+    await attemptFetchingUserMissions(emit);
+  }
+
+  //! Méthode permettant de récupérer les missions de l'utilisateur
+  Future<void> attemptFetchingUserMissions(
+    Emitter<MediplanState> emit,
+  ) async {
+    final List<Mission> missions = [];
+
+    emit(state.copyWith(mediplanStatus: MediplanLoadingStatus()));
+
+    try {
+      final token = await secureStorage.read(key: 'jwt');
+      final userId = await secureStorage.read(key: 'userId');
+
+      final response = await mediplanRepository.getUserMissions(
+        userId: userId!,
+        token: token!,
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body)["data"];
+
+        if (data.length != 0) {
+          for (var i = 0; i < data.length; i++) {
+            missions.add(Mission.fromJson(data[i]));
+          }
+        }
+        if (data.length > 1) {
+          missions.sort((a, b) {
+            // Compare by last time studied
+            return a.start.compareTo(b.start);
+          });
+        }
+
+        emit(
+          state.copyWith(
+            missions: missions,
+            mediplanStatus: MediplanSuccessStatus(),
+          ),
+        );
+      } else {
+        emit(
+          state.copyWith(
+            mediplanStatus: MediplanErrorStatus(
+              Exception(
+                  "Erreur lors de la récupération des missions de l'utilisateur."),
+            ),
+          ),
+        );
+      }
+    } catch (error) {
+      emit(
+        state.copyWith(
+          mediplanStatus: MediplanErrorStatus(
+            Exception(error),
+          ),
+        ),
+      );
+    }
   }
 
   //! Méthode permettant de récupérer les aides-soignants
--- lib/blocs/mediplan_bloc/mediplan_state.dart
+++ lib/blocs/mediplan_bloc/mediplan_state.dart
@@ -1,23 +1,28 @@
 import 'package:mediplan/blocs/mediplan_bloc/mediplan_status.dart';
+import 'package:mediplan/models/mission.dart';
 import 'package:mediplan/models/user.dart';
 
 class MediplanLoadingState {}
 
 class MediplanState {
   final List<User>? caregivers;
+  final List<Mission>? missions;
   final MediplanStatus mediplanStatus;
 
   MediplanState({
     this.caregivers = const [],
+    this.missions = const [],
     this.mediplanStatus = const MediplanInitialStatus(),
   });
 
   MediplanState copyWith({
     List<User>? caregivers,
+    List<Mission>? missions,
     MediplanStatus? mediplanStatus,
   }) {
     return MediplanState(
       caregivers: caregivers ?? this.caregivers,
+      missions: missions ?? this.missions,
       mediplanStatus: mediplanStatus ?? this.mediplanStatus,
     );
   }
